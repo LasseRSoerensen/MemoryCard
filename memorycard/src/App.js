9e7dc3b1c9586fd484e506286e328951443a823d
Lasse Sørensen <61781595+LasseRSoerensen@users.noreply.github.com>
2023-07-01 17:58:35 +0200

    Worked on states, and alignment of cards

--- memorycard/src/App.js
+++ memorycard/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from './components/Card';
+import React, { useState } from "react";
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [pool, setPool] = useState([]);
 {/* put here array to keep track of all cards present from the card pool, so the same 
 is not displayed twice. Also keep track of score and user pick */}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-row">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+  
     </div>
   );
 }
--- memorycard/src/components/Card.js
+++ memorycard/src/components/Card.js
@@ -1,1 +1,19 @@
+import { useState } from "react";
+
 {/* should contain states of the picture, if it has been clicked, if it has been clicked before */}
+
+function Card() {
+    const [image, setImage] = useState(""); 
+
+    {/* make the image div clickable and have a function to run when clicked */}
+
+    {/* put here array to keep track of all cards present from the card pool, so the same 
+    is not displayed twice. Also keep track of score and user pick */}
+      return (
+        <div className="bg-green-100 border-2">
+            this is a test
+        </div>
+      );
+    }
+    
+export default Card;
