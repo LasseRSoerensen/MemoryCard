8e4295c7c9bdf95e91d71267c47c06017b54fa34
LasseRSoerensen <61781595+SquintyShip@users.noreply.github.com>
2023-07-02 13:02:08 +0200

    worked on alignment

--- memorycard/src/App.js
+++ memorycard/src/App.js
@@ -2,22 +2,17 @@
 import './App.css';
 import Card from './components/Card';
 import React, { useState } from "react";
-
+import Header from './components/Header';
+import Main from './components/Main';
 function App() {
   const [cards, setCards] = useState([]);
   const [pool, setPool] = useState([]);
 {/* put here array to keep track of all cards present from the card pool, so the same 
-is not displayed twice. Also keep track of score and user pick */}
+is not displayed twice. Also keep track of score and user pick     <div className="App flex flex-wrap gap-4 h-screen flex-row items-center"> */}
   return (
-    <div className="App flex flex-row">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+    <div className="App border-2 border-black">
+      <Header/>
+      <Main/>
   
     </div>
   );
--- memorycard/src/components/Main.js
+++ memorycard/src/components/Main.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-
+import Card from "./Card";
 {/* should contain states of the picture, if it has been clicked, if it has been clicked before */}
 
-function Card() {
+function Main() {
     const [image, setImage] = useState(""); 
 
     {/* make the image div clickable and have a function to run when clicked */}
@@ -10,10 +10,17 @@ function Card() {
     {/* put here array to keep track of all cards present from the card pool, so the same 
     is not displayed twice. Also keep track of score and user pick */}
       return (
-        <div className="bg-green-100 border-2 border-red-500 basis-100 self-start basis-40 h-40 ">
-            this is a test
+        <div className="border-2 border-black h-screen flex-wrap flex flex-row content-center justify-center gap-2">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
       );
     }
     
-export default Card;
+export default Main;
