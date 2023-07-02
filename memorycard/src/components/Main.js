33e0c424a069b4c578bddb741d671b7f99557ad4
LasseRSoerensen <61781595+SquintyShip@users.noreply.github.com>
2023-07-02 16:49:06 +0200

    implemented image passing with props

--- memorycard/src/components/Main.js
+++ memorycard/src/components/Main.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import Card from "./Card";
+import birds from '../cards/birds.jpg';
+import cows from '../cards/cow.jpg';
+import leopard from '../cards/leopard.jpg';
+import pinguin from '../cards/pinguin.jpg';
 {/* should contain states of the picture, if it has been clicked, if it has been clicked before */}
 
 function Main() {
@@ -12,8 +16,8 @@ function Main() {
       return (
         <div className="border-2 border-black h-screen flex-wrap flex flex-row content-center justify-center gap-2">
             <Card/>
-            <Card/>
-            <Card/>
+            <Card img={birds}/>
+            <Card img={cows}/>
             <Card/>
             <Card/>
             <Card/>
